def7159fdb41096e92700ef3540770772b2ec644
ana.cecilia@gec.inate.br <ana.cecilia@gec.inatel.br>
2023-11-09 10:43:18 -0300

    ceci teste

--- CinemaList/src/app/lista-filmes/lista-filmes.component.spec.ts
+++ CinemaList/src/app/lista-filmes/lista-filmes.component.spec.ts
@@ -28,9 +28,9 @@ describe('ListaFilmesComponent', () => {
     component.newFilme.genero = 'Ação';
     component.newFilme.ano = '2022';
     component.newFilme.nota = '4.5';
-
-    component.onAddMovieClick();
-
+  
+    const result = component.onAddMovieClick();
+  
     expect(component.filmes.length).toBe(1);
     expect(component.filmes[0]).toEqual({
       nome: 'Filme 1',
@@ -38,32 +38,53 @@ describe('ListaFilmesComponent', () => {
       ano: '2022',
       nota: '4.5',
     });
+  
+    expect(component.newFilme).toEqual({ nome: '', genero: '', ano: '', nota: '' });
+  
+    expect(result).toBe('Filme adicionado com sucesso!');
   });
-
+  
 
   it('should not add a movie with invalid data', () => {
     component.newFilme.nome = '';
     component.newFilme.genero = '';
     component.newFilme.ano = '';
     component.newFilme.nota = '';
-
-    component.onAddMovieClick();
-
+  
+    const result = component.onAddMovieClick();
+  
     expect(component.filmes.length).toBe(0);
+  
+  expect(result).toBe('Por favor, preencha todos os campos obrigatórios.');
   });
+  
   it('should remove a movie', () => {
-    component.filmes = [{
-      nome: 'Filme 1',
-      genero: 'Ação',
-      ano: '2022',
-      nota: '4.5',
-    }];
+    const initialMovies = [
+      {
+        nome: 'Filme 1',
+        genero: 'Ação',
+        ano: '2022',
+        nota: '4.5',
+      },
+      {
+        nome: 'Filme 2',
+        genero: 'Comédia',
+        ano: '2021',
+        nota: '3.8',
+      },
+    ];
   
-    component.deleteFilme(component.filmes[0]);
+    component.filmes = [...initialMovies];
+    
+    const movieToRemove = initialMovies[0];
+    const expectedMoviesAfterRemoval = [initialMovies[1]];
   
-
-    expect(component.filmes.length).toBe(0);
+    const result = component.deleteFilme(movieToRemove);
+  
+    expect(component.filmes).toEqual(expectedMoviesAfterRemoval);
+    expect(result).toBe('Filme excluído com sucesso!');
   });
+  
   
 
 });
